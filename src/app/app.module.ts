473b792e118e4a115b68e958d9afb43972518a1a
Yomar Miranda <mirandayomar@gmail.com>
2018-07-02 01:26:16 -0500

    Cargando nuestra nueva página a la App.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LugarPage } from '../pages/lugar/lugar';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LugarPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LugarPage
   ],
   providers: [
     StatusBar,
