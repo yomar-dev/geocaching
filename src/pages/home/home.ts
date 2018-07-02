a963034160613104b9bfbb00f7919c3bee4cfa90
Yomar Miranda <mirandayomar@gmail.com>
2018-07-02 01:39:22 -0500

    Creando un boton para navegar hacia otra página.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LugarPage } from '../lugar/lugar';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  irLugar(){
+   this.navCtrl.push(LugarPage); 
   }
 
 }
